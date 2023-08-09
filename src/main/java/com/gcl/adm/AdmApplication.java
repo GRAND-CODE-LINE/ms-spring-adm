7ee08e21fd655139735a508b69734c1383d05d1e
ConsultorCarlos <52987839+cmb1197@users.noreply.github.com>
2023-08-08 22:22:29 -0500

    libreria ahora si

--- src/main/java/com/gcl/adm/AdmApplication.java
+++ src/main/java/com/gcl/adm/AdmApplication.java
@@ -1,17 +1,10 @@
 package com.gcl.adm;
-
-import java.text.SimpleDateFormat;
-import java.time.Period;
-import java.time.temporal.ChronoUnit;
-import java.util.Date;
-import java.util.Scanner;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
+@EnableMongoRepositories(basePackages = "com.gcl.dental.core")
 @SpringBootApplication
-@ComponentScan("com.gcl.dental.core")
 public class AdmApplication {
 
 	public static void main(String[] args) {
